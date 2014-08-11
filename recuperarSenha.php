fb79710edfd230d3feae83f617997d0b6383c82b
EQUIPE1_3F <EQUIPE1_3F@LAB2M04.itbeng.inf>
2014-08-11 09:58:14 -0300

    teste github

--- recuperarSenha.php
+++ recuperarSenha.php
@@ -1,15 +1,18 @@
 <?php
 echo '<meta charset="UTF-8">';
-
+include 'conexao/conecta.inc';
+if(isset($_POST['recupera'])){
 function geraSaltAleatorio($tamanho = 22) {
 return substr(sha1(mt_rand()), 0, $tamanho); 
 }
 $salt = geraSaltAleatorio();
 // Senha do usuário, pode ter vindo do $_POST, $_GET ou outro lugar
+$email = $_POST['email'];
 $query = "SELECT * FROM usuarios WHERE EMAIL_USUARIO = '$email' ";
 $result= mysql_query($query);
 $usuarios = mysql_fetch_assoc($result);
 $senha = $usuarios['SENHA_USUARIO'];
+echo $senha;
 // Cria um hash
 $hash = md5($senha . $salt);
 // Encripta esse hash 1000 vezes
@@ -17,11 +20,14 @@ for ($i = 0; $i < 1000; $i++) {
 $hash = md5($hash);
 }
 $email = $_POST['email'];
-$query = "UPDATE SENHA_USUARIO SET usuarios VALUES('$hash') WHERE EMAIL_USUARIO= $email";
-if(mysql_query($query)){
+$sql = "UPDATE SENHA_USUARIO SET usuarios VALUES('$hash') WHERE EMAIL_USUARIO= $email";
+if(mysql_query($sql)){
  
 echo 'Sua nova senha é : '.substr($hash,0,7);
  
 }else {
     
+}
+}else{
+    header('Location:frmRecuperaSenha.php');
 }
